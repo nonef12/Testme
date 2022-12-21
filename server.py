47fdce33a32e0131b01e36f7ffd331b070b1ba55
nonef12 <59384663+nonef12@users.noreply.github.com>
2022-12-21 12:40:25 +0700

    Update server.py

--- server.py
+++ server.py
@@ -3,7 +3,9 @@ server = Flask(__name__)
 
 @server.route("/")
 def hello():
-    return "no"
+    i=0
+    while 1:
+       return i+=1
 
 if __name__ == "__main__":
    server.run(host='0.0.0.0', port=1337)
